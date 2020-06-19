07e4064063dbe959e8983c4fd1d7f2a0d9587f7f
jakub104 <jakubzlezena@wp.pl>
2020-06-19 21:20:36 +0200

    Updates

---
+++ precache-manifest.77b22d3bd742345be43e6d9cbded05e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b7ee3ff216287758d23a2d8d60d960",
+    "url": "/kuc/index.html"
+  },
+  {
+    "revision": "559671a78320e9b5e23e",
+    "url": "/kuc/static/css/main.5b358ffa.chunk.css"
+  },
+  {
+    "revision": "85cba47260f6827fe80e",
+    "url": "/kuc/static/js/2.5158eda6.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "/kuc/static/js/2.5158eda6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "559671a78320e9b5e23e",
+    "url": "/kuc/static/js/main.246a896b.chunk.js"
+  },
+  {
+    "revision": "965547c8932b4950611e",
+    "url": "/kuc/static/js/runtime-main.6c067f30.js"
+  },
+  {
+    "revision": "ca28711aeb2b0f89e70cea20c4ceb6a8",
+    "url": "/kuc/static/media/address.ca28711a.png"
+  },
+  {
+    "revision": "30f40479abdee0eef3054b5505b95a31",
+    "url": "/kuc/static/media/logo.30f40479.png"
+  }
+]);
